c1154d67329fcdcdfba7ef3ded013f6de1ee1e7c
xiokevc <xiokevc@gmail.com>
2025-08-13 11:19:49 -0400

    trackschema

---
+++ models/Track.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const trackSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  artist: {
+    type: String,
+    required: true,
+  }
+});
+
+module.exports = mongoose.model('Track', trackSchema);
